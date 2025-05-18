93f336363e72c91f09665b89feaeae15e068b2e6
Murielle Beaulieu <muriellebeaulieu@Murielles-MacBook-Pro.local>
2025-05-19 09:50:36 +1000

    initial commit

---
+++ superhero-api/src/main/java/io/nology/superhero_api/SuperheroApiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.superhero_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroApiApplication.class, args);
+	}
+
+}
