029ffbc20a152876de73926f1ded3d049a8c1907
Murielle Beaulieu <muriellebeaulieu@Murielles-MacBook-Pro.local>
2025-06-28 22:52:19 +1000

    added homepage and carousel styling

--- front-end/src/components/SuperheroCard/SuperheroCard.tsx
+++ front-end/src/components/SuperheroCard/SuperheroCard.tsx
@@ -1,10 +1,6 @@
 import { useModal } from "../../context/ModalContext";
 import type { Superhero } from "../../services/superheroes-services";
 import styles from "./SuperHeroCard.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons"; // Import specific icons
-import { createSuperheroFavourite } from "../../services/favourites-services";
-import { useMutation } from "@tanstack/react-query";
 
 export interface dbHero {
   name: string;
@@ -23,20 +19,6 @@ export const SuperHeroCard = ({ superhero }: SuperheroCardProps) => {
     setModalHero(modalHero);
   };
 
-  const addFavouriteMutation = useMutation({
-    mutationFn: createSuperheroFavourite,
-    onSuccess: () => {
-      console.log("yay");
-    },
-    onError: (err) => {
-      console.log(err.message);
-    },
-  });
-
-  const addToFavourites = (selectedHero: Superhero) => {
-    addFavouriteMutation.mutate(selectedHero);
-  };
-
   if (superhero) {
     return (
       <div
@@ -44,16 +26,11 @@ export const SuperHeroCard = ({ superhero }: SuperheroCardProps) => {
         key={superhero.name}
         onClick={() => openModal(superhero)}
       >
-        {/* I would want the image of the superhero as a cover and on hover the name and "click to see more" */}
-        <p>{superhero.name}</p>
-        <div className={styles.quickFav}>
-          <div className={styles.icon}>
-            <FontAwesomeIcon
-              icon={faHeart}
-              onClick={() => addToFavourites(superhero)}
-            />
-          </div>
-        </div>
+        <img src={superhero.images.sm}></img>
+        <span className={styles.info}>
+          <h2 className={styles.see_more}>See more</h2>
+          <h2>{superhero.name}</h2>
+        </span>
       </div>
     );
   }
--- front-end/src/pages/Homepage.tsx
+++ front-end/src/pages/Homepage.tsx
@@ -2,13 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchAllSuperheroes } from "../services/superheroes-services";
 import { Navbar } from "../components/Navbar/Navbar";
 import { NavButton } from "../components/NavButton/NavButton";
-import { Hero } from "../components/Hero/Hero";
+import { HeroBanner } from "../components/HeroBanner/HeroBanner";
 import { Carousel } from "../components/Carousel/Carousel";
 import { useNavigate } from "react-router";
 import { useModal } from "../context/ModalContext";
 import { SuperheroModal } from "../components/SuperheroModal/SuperheroModal";
 import { useCarousel } from "../context/CarouselContext";
-import { useState } from "react";
+import { useEffect } from "react";
 
 export const Homepage = () => {
   const { data, isLoading, isSuccess, isError, error } = useQuery({
@@ -16,13 +16,22 @@ export const Homepage = () => {
     queryFn: fetchAllSuperheroes,
   });
 
-
   const navigate = useNavigate();
 
   const { modalOpen } = useModal();
+  const { savedDate, today } = useCarousel();
+  let randomStartingIndex = Number(localStorage.getItem("randomIndex"));
 
-  const [slice, setSlice] = useState<string[] | undefined>(undefined)
-  const {today, date} = useCarousel();
+  useEffect(() => {
+    if (randomStartingIndex == null) {
+      randomStartingIndex = Math.floor(Math.random() * data.length);
+      localStorage.setItem("randomIndex", `${randomStartingIndex}`);
+    }
+    if (savedDate != today) {
+      randomStartingIndex = Math.floor(Math.random() * data.length);
+      localStorage.setItem("randomIndex", `${randomStartingIndex}`);
+    }
+  }, [data, isSuccess, today, randomStartingIndex]);
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -33,27 +42,12 @@ export const Homepage = () => {
   }
 
   if (isSuccess) {
-
-  const randomStartingIndex = Math.floor(Math.random() * data.length);
-
-  localStorage.setItem("randIndex", `${randomStartingIndex}`);
-
-  if (today == date) {
-    // const slice1 = +localStorage.getItem("randIndex");
-    // const slice2 = +slice1 + 5;
-    //   setSlice([slice1, slice2])
-    // console.log(slice);
-    console.log(true);
-  } else {
-    //   localStorage.setItem("randIndex", `${Math.floor(Math.random() * data.length)}`);
-    //  console.log("changed date so new index: " +localStorage.getItem("randIndex"))
-    console.log(false)
-  }
-
     const carouselHeroesSlice = data.slice(
       randomStartingIndex,
-      randomStartingIndex + 5
+      Number(randomStartingIndex) + 5
     );
+
+    console.log(carouselHeroesSlice);
 
     return (
       <>
@@ -65,12 +59,7 @@ export const Homepage = () => {
             />
           }
         </Navbar>
-        <Hero />
-        <NavButton
-          handleClick={() => navigate("/all")}
-          innerText={"Browse All"}
-        />
-        <h2>Superheroes of the day</h2>
+        <HeroBanner />
         <Carousel carouselHeroesSlice={carouselHeroesSlice} />
         <header></header>
         {modalOpen && <SuperheroModal />}
